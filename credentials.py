a2c244d1083a22d83040b20d6215f9e0a4e10016
IceFlamesStudio <ice-flamesstudio@outlook.com>
2023-02-13 15:31:31 +0200

    asd

--- credentials.py
+++ credentials.py
@@ -1,5 +1,5 @@
 # User email: 
-email_user = 'check exist'
+email_user = 'not exist yet'
 # User password: 
 email_password = ''
 # Server for Office, details -> https://domar.com/pages/smtp_pop3_server 
